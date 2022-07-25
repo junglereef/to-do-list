e433921b0fff727d113cf4a6b7ce36fad3892e95
Felipe Alexandre André <felipeandree8@outlook.com>
2022-07-25 20:13:25 -0300

    input update

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Todolist from './components/Todolist';
 
 function App() {
   const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Seven Dev's Todo List</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText} />
       <Todolist />
     </div>
   );
